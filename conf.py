cd793500c693f972bca3bfb3b83611417a44a400
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-03 19:29:31 +0000

    Add sphinx-needs

--- conf.py
+++ conf.py
@@ -6,23 +6,24 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Our new car'
-copyright = '2025, michaelmnemonic'
-author = 'michaelmnemonic'
-release = '0.1.final_2'
+project = "Our new car"
+copyright = "2025, michaelmnemonic"
+author = "michaelmnemonic"
+release = "0.1.final_2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_needs",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
