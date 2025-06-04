4a51f1e8df7f615b440bbd7b8e0503ee55f92f29
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-04 22:13:30 +0200

    Add flake devshell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nix configuration for experiements with sphinx-needs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShell = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+    in
+      pkgs.mkShell {
+        buildInputs = [
+        (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.sphinx
+            ]))
+      });
+  };
+}
